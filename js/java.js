71f09e409270d5c12aff3c679dad46e537608abc
Nassim <101125927+Nassim-H@users.noreply.github.com>
2023-01-22 20:12:09 +0100

    Ajout
    Ajout de fonctionnalités

--- js/java.js
+++ js/java.js
@@ -1,6 +1,3 @@
-var app = new Vue({
-  el: '#app',
-  data: {
-    message: 'Hello Vue !'
-  }
-})
+
+
+const presente = "Fou amoureux de l'adrénaline, j'aime me surpasser dans tout ce que j'entreprends, j'entreprends donc ici de vous surprendre. Il est dit que les apparences sont parfois trompeuses, je ne sais pas si cela est vrai, mais j'espère que cela va vous surprendre !"
